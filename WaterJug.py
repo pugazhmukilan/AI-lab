877308c598af2151c4a4f477cd12486d92432552
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2024-02-14 08:14:59 +0530

    tic tac tao

--- WaterJug.py
+++ WaterJug.py
@@ -3,8 +3,7 @@ from collections import defaultdict
 
 jug1 =4
 jug2 = 3
-aim = 2
-
+final =2
 #initilizing  the matrix to default False
 
 visited  = defaultdict(lambda : False)
@@ -13,16 +12,18 @@ def Solve_Jug( a1,a2):
     
    
     
-    if (a1==aim and a2==0)or(a2==aim and a1==0):
+    if (a1==final and a2==0)or(a2==final and a1==0):
         
-        print(a1,a2)
+        print("We reached the GOAL STATE!!!  ",a1,a2)
         return True
         
     if (visited[(a1,a2)]==False) :
         print(a1,a2)
         visited[(a1,a2)] =True
-            
-        return( Solve_Jug(0,a2)or Solve_Jug(a1,0) or Solve_Jug(jug1,a2) or Solve_Jug(a1,jug2) or Solve_Jug(jug1,a2-(4-a1)if a2-(4-a1)>=0 else 0) or Solve_Jug(a1-(3-a2) if a1-(3-a2)>=0 else 0,3)or Solve_Jug(a1+a2,0) or Solve_Jug(0,a1+a2))
+        #in this return statement something will return true and that will be taken
+        return( Solve_Jug(0,a2)or Solve_Jug(a1,0) or Solve_Jug(jug1,a2) or Solve_Jug(a1,jug2) or 
+                Solve_Jug(jug1,a2-(4-a1)if a2-(4-a1)>=0 else 0) or Solve_Jug(a1-(3-a2) if a1-(3-a2)>=0 else 0,3)or 
+                Solve_Jug(a1+a2 if (a1+a2<=jug1) else jug1,0) or Solve_Jug(0,a1+a2 if(a1+a2<=jug2) else jug2))
     else:
         return False
 print("Solution: =================================")
